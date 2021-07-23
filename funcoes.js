e0b982815a3b80be39a846efaeaaee27ac6da797
Yuri Castro Najar <yuricn999@gmail.com>
2021-07-23 16:14:54 -0300

    Ajuste local storage

--- funcoes.js
+++ funcoes.js
@@ -1,87 +1,120 @@
 $(function(){
-    var operacao = "A"; //"A"=Adição; "E"=Edição
-    var indice_selecionado = -1; //Índice do item selecionado na lista
-    var tbClientes = localStorage.getItem("tbClientes");// Recupera os dados armazenados
-    tbClientes = JSON.parse(tbClientes); // Converte string para objeto
-    if(tbClientes == null) // Caso não haja conteúdo, iniciamos um vetor vazio
-    tbClientes = [];
-});
 
-function Adicionar(){
-    var cliente = JSON.stringify({
-        Nome     : $("#txtNome").val(),
-        Telefone : $("#txtFone").val(),
-        Email    : $("#txtEmail").val()
-    });
-    tbClientes.push(cliente);
-    localStorage.setItem("tbClientes", JSON.stringify(tbClientes));
-    alert("Registro adicionado.");
-    return true;
-}
+	var operacao = "A"; //"A"=Adição; "E"=Edição
 
-function Editar(){
-    tbClientes[indice_selecionado] = JSON.stringify({
-            Nome     : $("#txtNome").val(),
-            Telefone : $("#txtFone").val(),
-            Email    : $("#txtEmail").val()
-        });//Altera o item selecionado na tabela
-    localStorage.setItem("tbClientes", JSON.stringify(tbClientes));
-    alert("Informações editadas.")
-    operacao = "A"; //Volta ao padrão
-    return true;
-}
+	var indice_selecionado = -1;
 
-function Excluir(){
-    tbClientes.splice(indice_selecionado, 1);
-    localStorage.setItem("tbClientes", JSON.stringify(tbClientes));
-    alert("Registro excluído.");
-}
+	var tbClientes = localStorage.getItem("tbClientes");// Recupera os dados armazenados
 
-function Listar(){
-    $("#tblListar").html("");
-    $("#tblListar").html(
-        "<thead>"+
-        "   <tr>"+
-        "   <th></th>"+
-        "   <th>Nome</th>"+
-        "   <th>Telefone</th>"+
-        "   <th>Email</th>"+
-        "   </tr>"+
-        "</thead>"+
-        "<tbody>"+
-        "</tbody>"
-        );
-    for(var i in tbClientes){
-        var cli = JSON.parse(tbClientes[i]);
-        $("#tblListar tbody").append("<tr>");
-        $("#tblListar tbody").append("<td><img src='edit.png' alt='"+i+"'class='btnEditar'/><img src='delete.png' alt='"+i+"' class='btnExcluir'/></td>");
-        $("#tblListar tbody").append("<td>"+cli.Nome+"</td>");
-        $("#tblListar tbody").append("<td>"+cli.Telefone+"</td>");
-        $("#tblListar tbody").append("<td>"+cli.Email+"</td>");
-        $("#tblListar tbody").append("</tr>");
-    }
-}
+	tbClientes = JSON.parse(tbClientes); // Converte string para objeto
 
-$("#frmCadastro").on("submit",function(){
-    if(operacao == "A")
-        return Adicionar();
-    else
-        return Editar();
-});
+	if(tbClientes == null) // Caso não haja conteúdo, iniciamos um vetor vazio
+		tbClientes = [];
 
+	function Adicionar(){
+		var cli = GetCliente("Codigo", $("#txtCodigo").val());
 
-$("#tblListar").on("click", ".btnEditar", function(){
-    operacao = "E";
-    indice_selecionado = parseInt($(this).attr("alt"));
-    var cli = JSON.parse(tbClientes[indice_selecionado]);
-    $("#txtNome").val(cli.Nome);
-    $("#txtTelefone").val(cli.Telefone);
-    $("#txtEmail").val(cli.Email);
-    $("#txtNome").focus();
-});
+		if(cli != null){
+			alert("Código já cadastrado.");
+			return;
+		}
 
-$("#tblListar").on("click", ".btnExcluir",function(){
-    indice_selecionado = parseInt($(this).attr("alt"));
-    Excluir();
-    Listar();
+		var cliente = JSON.stringify({
+			Codigo   : $("#txtCodigo").val(),
+			Nome     : $("#txtNome").val(),
+			Telefone : $("#txtFone").val(),
+			Email    : $("#txtEmail").val()
+		});
+
+		tbClientes.push(cliente);
+
+		localStorage.setItem("tbClientes", JSON.stringify(tbClientes));
+
+		alert("Registro adicionado.");
+		return true;
+	}
+
+	function Editar(){
+		tbClientes[indice_selecionado] = JSON.stringify({
+				Codigo   : $("#txtCodigo").val(),
+				Nome     : $("#txtNome").val(),
+				Telefone : $("#txtFone").val(),
+				Email    : $("#txtEmail").val()
+			});
+		localStorage.setItem("tbClientes", JSON.stringify(tbClientes));
+		alert("Informações editadas.")
+		operacao = "A";
+		return true;
+	}
+
+	function Listar(){
+		$("#tblListar").html("");
+		$("#tblListar").html(
+			"<thead>"+
+			"	<tr>"+
+			"<th></th>"+
+			"	<th>Código</th>"+
+			"	<th>Nome</th>"+
+			"	<th>Telefone</th>"+
+			"	<th>Email</th>"+
+			"	</tr>"+
+			"</thead>"+
+			"<tbody>"+
+			"</tbody>"
+			);
+
+		 for(var i in tbClientes){
+			var cli = JSON.parse(tbClientes[i]);
+		  	$("#tblListar tbody").append("<tr>"+
+									 	 "	<td><img src='img/edit.png' alt='"+i+"' class='btnEditar'/><img src='img/delete.png' alt='"+i+"' class='btnExcluir'/></td>" + 
+										 "	<td>"+cli.Codigo+"</td>" + 
+										 "	<td>"+cli.Nome+"</td>" + 
+										 "	<td>"+cli.Telefone+"</td>" + 
+										 "	<td>"+cli.Email+"</td>" + 
+		  								 "</tr>");
+		 }
+	}
+
+	function Excluir(){
+		tbClientes.splice(indice_selecionado, 1);
+		localStorage.setItem("tbClientes", JSON.stringify(tbClientes));
+		alert("Registro excluído.");
+	}
+
+	function GetCliente(propriedade, valor){
+		var cli = null;
+        for (var item in tbClientes) {
+            var i = JSON.parse(tbClientes[item]);
+            if (i[propriedade] == valor)
+                cli = i;
+        }
+        return cli;
+	}
+
+	Listar();
+
+	$("#frmCadastro").on("submit",function(){
+		if(operacao == "A")
+			return Adicionar();
+		else
+			return Editar();		
+	});
+
+	$("#tblListar").on("click", ".btnEditar", function(){
+		operacao = "E";
+		indice_selecionado = parseInt($(this).attr("alt"));
+		var cli = JSON.parse(tbClientes[indice_selecionado]);
+		$("#txtCodigo").val(cli.Codigo);
+		$("#txtNome").val(cli.Nome);
+		$("#txtFone").val(cli.Telefone);
+		$("#txtEmail").val(cli.Email);
+		$("#txtCodigo").attr("readonly","readonly");
+		$("#txtNome").focus();
+	});
+
+	$("#tblListar").on("click", ".btnExcluir", function(){
+		indice_selecionado = parseInt($(this).attr("alt"));
+		Excluir();
+		Listar();
+	});
 });
